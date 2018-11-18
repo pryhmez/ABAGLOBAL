677051e6b1b322eaa13fe134a2b4733e3138c0f8
pryhmez <edmundisaac16@gmail.com>
2018-11-18 01:13:54 +0100

    first commit

---
+++ app/src/main/java/com/example/prime/abawakasearch/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.prime.abawakasearch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                }
+            }
+        };timer.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
